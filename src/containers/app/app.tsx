ca119a1242c206373ac6f29e3d14069f2eb8c8b4
Ramsy de Vos <ramsy@ramsy.dev>
2020-12-06 18:27:29 +0100

    feat(app): add global styling

--- src/containers/app/app.tsx
+++ src/containers/app/app.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components'
 import { Login } from '../login';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: Inter,ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;
+    margin: 0;
+  }
+`
 
 function App() {
   return (
     <div>
+      <GlobalStyle />
       <Login />
     </div>
   );
