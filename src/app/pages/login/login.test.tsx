75e159b1c4042c55103636457cd79e9a55778149
Ramsy de Vos <ramsy@ramsy.dev>
2020-12-11 10:56:45 +0100

    test(login): include memory router

--- src/app/pages/login/login.test.tsx
+++ src/app/pages/login/login.test.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MemoryRouter } from 'react-router-dom';
 import { Login } from '.'
 
 describe('Login', () => {
     it('renders without crashing', () => {
         const div = document.createElement('div');
-        ReactDOM.render(<Login />, div);
+        ReactDOM.render(
+            <MemoryRouter><Login /></MemoryRouter>
+        , div);
         ReactDOM.unmountComponentAtNode(div);
     })
 })
