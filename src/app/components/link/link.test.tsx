0ec6b96a792b3bbe4e90d100d6128985ec69e2b3
Ramsy de Vos <ramsy@ramsy.dev>
2020-12-11 10:56:31 +0100

    test(link): include memory router

--- src/app/components/link/link.test.tsx
+++ src/app/components/link/link.test.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MemoryRouter } from 'react-router-dom';
 import { Link } from '.'
 
 describe('Link', () => {
     it('renders without crashing', () => {
         const div = document.createElement('div');
-        ReactDOM.render(<Link to="/" />, div);
+        ReactDOM.render(
+            <MemoryRouter><Link to="/" /></MemoryRouter>
+        , div);
         ReactDOM.unmountComponentAtNode(div);
     })
 })
