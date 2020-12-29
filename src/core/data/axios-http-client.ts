eb813e75e99cc42e2c4a7c7591cdfede462dfffb
Ramsy de Vos <ramsy@ramsy.dev>
2020-12-29 08:49:37 +0100

    chore(core): update axios-http-client

--- src/core/data/axios-http-client.ts
+++ src/core/data/axios-http-client.ts
@@ -3,28 +3,17 @@ import { HttpClient } from "../../../../../core/data/protocols";
 
 export class AxiosHttpClient extends HttpClient {
 
-    constructor(props: HttpClient.ConstructorProps) {
-        super(props);
-
-        this.baseUrl = "http://localhost:3000"
-    }
-
     post<P>(path: string, payload: HttpClient.Post): Promise<P> {
-        return axios.post(
-            this.baseUrl + path,
-            payload,
-            {
-                headers: payload.headers
-            }
-        )
-    }
-    delete<D>(path: string, payload: HttpClient.Delete): Promise<D> {
-        return axios.delete(
-            this.baseUrl +  path,
-            {
-                headers: payload.headers
-            }
-        )
-    }
+        const { body, headers } = payload;
 
+        return axios.post(
+            path,
+            body,
+            {
+                headers,
+                baseURL: this.baseUrl,
+                withCredentials: true
+            }
+        )
+    }
 }
