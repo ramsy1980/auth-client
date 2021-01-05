b70b9ee6f2d1d4b516b99f34deee1e5c20436429
Ramsy de Vos <ramsy@ramsy.dev>
2021-01-05 14:10:34 +0100

    refactor(components): add index.ts

---
+++ src/app/components/index.ts
@@ -0,0 +1,7 @@
+export * from './button';
+export * from './checkbox';
+export * from './container';
+export * from './form';
+export * from './header';
+export * from './input-field';
+export * from './link';
