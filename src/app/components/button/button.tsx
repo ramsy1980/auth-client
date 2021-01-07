478cd74246b8decaabbbf9889ee7ad8e8806bc51
Ramsy de Vos <ramsy@ramsy.dev>
2021-01-07 08:37:36 +0100

    feat(button): update button
    add loading and disabled state

--- src/app/components/button/button.tsx
+++ src/app/components/button/button.tsx
@@ -1,23 +1,30 @@
-import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { faSpinner, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 import { ButtonHTMLAttributes, FunctionComponent } from 'react';
 import { StyledButton, StyledButtonIcon } from './styled';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     icon?: IconDefinition
     title: string;
+    isLoading?: boolean;
 }
 
 export const Button: FunctionComponent<ButtonProps> = (props) => {
 
-    const { icon, type, title, ...rest } = props;
+    const { icon, type, title, isLoading, ...rest } = props;
+
+    const renderIcon = () => {
+        if (isLoading) {
+            return <StyledButtonIcon icon={faSpinner} spin={true} />
+        }
+        if (icon) {
+            return <StyledButtonIcon icon={icon} />
+        }
+        return null;
+    }
 
     return (
-        <StyledButton type={type} {...rest}>
-            {
-                icon && (
-                    <StyledButtonIcon icon={icon} />
-                )
-            }
+        <StyledButton type={type} {...rest} disabled={isLoading}>
+            {renderIcon()}
             <span>{title}</span>
         </StyledButton>
     )
--- src/app/components/button/styled.ts
+++ src/app/components/button/styled.ts
@@ -1,15 +1,21 @@
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { ButtonHTMLAttributes } from 'react';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
 
 export const StyledButton = styled.button`
     --bg-opacity: 1; // bg-indigo-600
     --text-opacity: 1; // text-white
+    --text-opacity-disabled: .5;
+    --text-white: rgba(255, 255, 255, var(--text-opacity));
+    --text-disabled: rgba(255, 255, 255, var(--text-opacity-disabled));
     align-items: center;
     background-color: rgba(79, 70, 229, var(--bg-opacity)); // bg-indigo-600
     border-color: transparent;
     border-radius: 0.375rem;
-    color: rgba(255, 255, 255, var(--text-opacity)); // text-white
-    cursor: pointer;
+    color: ${(props: ButtonProps) => props.disabled ? 'var(--text-disabled)' : 'var(--text-white)'}; // text-white
+    cursor: ${(props: ButtonProps) => props.disabled ? 'not-allowed' : 'pointer'};
     display: grid;
     font-size: 0.875rem;
     font-weight: 500;
@@ -23,12 +29,14 @@ export const StyledButton = styled.button`
 
     &:hover {
         --bg-opacity: 1; // bg-indigo-700
-        background-color: rgba(67, 56, 202, var(--bg-opacity)); // bg-indigo-700
+        /* background-color: rgba(67, 56, 202, var(--bg-opacity)); // bg-indigo-700 */
+        background-color: ${(props: ButtonProps) => !props.disabled && 'rgba(67, 56, 202, var(--bg-opacity))'};
         transition: all, 0.3s;
 
         & > svg {
             --text-opacity: 1; // text-indigo-300
-            color: rgba(165, 180, 252, var(--text-opacity)); // text-indigo-300
+            /* color: rgba(165, 180, 252, var(--text-opacity)); // text-indigo-300 */
+            color: ${(props: ButtonProps) => !props.disabled && 'rgba(165, 180, 252, var(--text-opacity))'};
             transition: all, 0.3s;
         }
     }
